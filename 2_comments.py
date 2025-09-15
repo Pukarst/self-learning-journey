26cba1881aa518366deaa161706ed3dc303ee40a
pukar_bade <bade5pukar@gmail.com>
2025-09-15 18:29:43 +0545

    Single & multi-line

---
+++ 2_comments.py
@@ -0,0 +1,23 @@
+'''
+It is a part of the coding file that the programmer doesn't wanted to execute,
+rather the programmer uses it to either explain a block of code or to avoid the execution of a specific part of code while testing.
+
+==> single line comment
+we use '#" to write the single line comment.
+
+==> multi-line comment
+we can use '#' in each line
+        OR
+we can use "....." or '..........' .(Both are same)
+
+'''
+
+print('This is the single line.')
+print()
+print('This is multiple-line.')
+
+print('''
+Twinkle twinkle little star
+How I wonder what you are 
+Up above the world so high
+Like a diamond in the sky.''')
