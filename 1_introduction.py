4cf27d430ff54610031fb490a5f84f52b32897ed
pukar_bade <bade5pukar@gmail.com>
2025-09-15 17:46:45 +0545

    Introduction and first program

---
+++ 1_introduction.py
@@ -0,0 +1,19 @@
+'''
+------------------------Introduction to Python----------------------
+
+Python is a high-level,interperted programming language known for it's 
+simplicity and readability.
+
+It was created by Guido Van Rossum and was released in 1991.
+
+Python is widely used in web-development,data science,machine learning,automation and many more.
+
+Its clean syntax makes it great for beginners, while its powerful libraries 
+and frameworks make it popular among professionals.
+
+
+'''
+
+'My first python program'
+
+print('This is my Python self-learning journey.')
