ce620a9d274960f99504130e3f31c871608e2d92
pukar_bade <bade5pukar@gmail.com>
2025-09-16 19:12:02 +0545

    Data Types

---
+++ 7_data_types.py
@@ -0,0 +1,17 @@
+'''
+Data types tell the computer what kind of data you're working with.
+
+Just like how we have different containers for water, sugar, or eggs — in programming, different data types hold different kinds of information.
+There are differnt types of data types in Python.
+We can get the data types of any objects by using the 'type()' function.
+'''
+name="Python"
+major=3
+minor_patch=13.5
+comp=1+9j
+a=True
+print(type(name),type(major),type(minor_patch),type(comp),type(a))
+
+'''
+There exist other data types also but those are the most common data types that are used in Python.
+'''
