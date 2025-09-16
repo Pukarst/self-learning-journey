00bea7a4f63cfc14d94b0c56f214e624d3590ca3
pukar_bade <bade5pukar@gmail.com>
2025-09-16 18:49:04 +0545

    Output Variables

---
+++ 6__output_variables.py
@@ -0,0 +1,13 @@
+a='Guido Van Rossum '
+b='developed Python in '
+c=1991
+d='1991'
+print(a,b,c)
+print()
+
+# '+' operator can only concate the same data type so we convert the '1991' into string
+print(a+b+d)
+
+e=679
+f=79
+print('The sum of the two given number are',e+f)
