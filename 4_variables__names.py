b4df700aa16f7e27c19cebdf355bde39de29033b
pukar_bade <bade5pukar@gmail.com>
2025-09-16 18:11:02 +0545

    Variables names

---
+++ 4_variables__names.py
@@ -0,0 +1,24 @@
+'''
+=========rules for naming the variables==========
++ can only contains the alpha-numeric characters and underscore (A-Z,a-z,0-9 and _)
++ must start with the letter or the underscore
++ are case sensitive (small and capital letters makes different)
++ can't start with the number
++ can't be any of the keywords
+
+======different technique of naming the variables==========
+1)camel case: each word except first letter start with capital letter
+2)pascal case: each word start with the capital letter
+3)snake case: each word is separated by underscore
+'''
+
+# camel case
+myVariable='python'
+
+#pascal case
+MyVariable='learning'
+
+#snake case
+my_variable='journey'
+
+print(myVariable,MyVariable,my_variable)
