1d280fe00ac463df62b18ebdff24ad382fdf0532
pukar_bade <bade5pukar@gmail.com>
2025-09-18 22:05:56 +0545

    string

---
+++ 9_string.py
@@ -0,0 +1,23 @@
+'''
+=String is the sequence of the characters like words,sentences,numbers or even the symbols that lies inside the single quotes ('....') or the double quotes ("....")
+=we can display it by using print() function.
+=we can assign the multiple line string by using three quotes.
+=we can assign a string to the variable name followed by the equal sign.
+'''
+
+# to assign the variable and print it
+name='Python' 
+print('The variable that we assign is',name)
+print()
+
+# multip-line string
+lorem='''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+'''
+print(lorem)
+
+
+'''
+we can find the length of the string by using the len() function'''
+# finding the length of the string in lorem variable
+print('The length of the string in the lorem variable is',len(lorem))
