89812c191a3f173f38488f569f7635f841f2763f
pukar_bade <bade5pukar@gmail.com>
2025-10-07 17:57:56 +0545

    String Methods

---
+++ 9.3_string.py
@@ -0,0 +1,14 @@
+'''
+There are different types of the string methods in Python. 
+They has a set of build in methods that we can use on the strings.
+All the strings methods return the new values and don't change the original string.
+The references can be obtained from the online sources.
+'''
+
+words='pneumonoultramicroscopicsilicovolcanoconiosis'
+a=words.capitalize()
+b=words.encode()
+c=words.upper()
+print(a)
+print(b)
+print(c)
