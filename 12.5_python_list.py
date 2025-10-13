93a2acc6631246e14fa8c1c5983a18de5abcf366
pukar_bade <bade5pukar@gmail.com>
2025-10-13 18:19:53 +0545

    Python list (loop)

---
+++ 12.5_python_list.py
@@ -0,0 +1,18 @@
+'''
+we can loop through the items by using the 'for' loop
+
+we can also loop through the list items by reffering to their index number. we can use range() and len() functions to create a suitable iterables.
+
+we can loop through the list items by using while() loop
+
+we will be discussing about the loop in further topic
+
+'''
+
+list_a=['key','lock','ram',951]
+for x in list_a:
+    print(x)
+
+
+for i in range(len(list_a)):
+    print(list_a[i])   
